99857564b0f25adf2f4ce9176c690c862b9ca614
Joswin Prince <87509443+joswinprince@users.noreply.github.com>
2022-07-02 19:34:40 +0530

    Update gitupdate.bat
    inputs from commandline

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,4 +1,6 @@
-git remote add upstream https://github.com/joswinprince/%2.git
+set /p account=Git Account: 
+set /p repo=Enter Repository: 
+git remote add upstream https://github.com/%account%/%repo%.git
 git pull upstream main
 git add .
 git commit -m %1
