400121bfda43311b22d05b0f2be37b9ecf99914e
Joswin Prince <87509443+joswinprince@users.noreply.github.com>
2022-07-02 19:36:25 +0530

    Update gitupdate.bat

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,7 +1,8 @@
 set /p account=Git Account: 
 set /p repo=Enter Repository: 
+set /p comments=Enter comments:
 git remote add upstream https://github.com/%account%/%repo%.git
 git pull upstream main
 git add .
-git commit -m %1
+git commit -m %comments%
 git push origin main
