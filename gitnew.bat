fe585f85bc2a9aa55fae669f5cac520669733ee9
Joswin Prince <87509443+joswinprince@users.noreply.github.com>
2022-07-02 19:46:43 +0530

    Update gitnew.bat

--- gitnew.bat
+++ gitnew.bat
@@ -1,7 +1,11 @@
-echo "# %1" >> README.md
+set /p account=Git Account: 
+set /p repo=Enter Repository: 
+set /p comments=Enter comments:
+echo "# %comments%" >> README.md
 git init
 git add .
 git commit -m "Initial commit"
 git branch -m main
-git remote add origin https://github.com/joswinprince/%1.git
+git remote add origin https://github.com/%account%/%repo%.git
 git push -u origin main
+
